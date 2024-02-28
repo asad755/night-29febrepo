aac5d3897412bd3db1b7e9efc728e3dbc17d9530
asad755 <143116178+asad755@users.noreply.github.com>
2024-02-29 01:10:27 +0500

    best

---
+++ app2.js
@@ -0,0 +1,14 @@
+// anonymous
+var teacher = {
+    name: "Zeeshan",
+    exp: 10
+};
+var student = {
+    name: "Hira",
+    age: 30
+};
+console.log(student["name"]);
+console.log(student.age);
+var storeManager = {
+    name: "Bilal"
+};
